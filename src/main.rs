1995ec9f29255d5abfd04ff837ff1e9fffe4a483
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-11-15 22:46:44 -0800

    Hello world, essentially

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+//! runst -- Runner for Rust "scripts"
+
+
+use std::env;
+use std::io::{self, Write};
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() == 0 {
+        panic!("No script name provided");
+    }
+    let ref script = args[0];
+
+    writeln!(&mut io::stderr(), "Running {}...", script).unwrap();
+
+    // TODO for V1:
+    // 1. create a temporary directory
+    // 2. symlink (or copy?) given file there
+    // 3. analyze it for extern-crate declarations
+    // 4. create a minimal Cargo.toml with those crates as [dependencies]
+    // 5. cd && cargo run
+
+    // V2:
+    // 1. use a single Cargo.toml "environment", with different scripts as [[bin]] entries
+    //   (to reuse compiled dependencies)
 }
