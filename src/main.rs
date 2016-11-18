b500e452f30a77f3d57fd927e32385dd8e8f5373
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-11-17 21:39:11 -0800

    Messing with logging

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
+//!
 //! runst -- Runner for Rust "scripts"
+//!
+
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate slog;
+             extern crate slog_stdlog;
+             extern crate slog_term;
+
+// `log` crate has to after `slog` as we want to use logging macros from `log`.
+#[macro_use] extern crate log;
 
 
 use std::env;
 use std::io::{self, Write};
+use std::path::PathBuf;
+
+
+lazy_static! {
+    /// Application version, as filled out by Cargo.
+    pub static ref VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+}
+
+lazy_static! {
+    /// Main application's directory.
+    static ref APP_DIR: PathBuf =
+        env::home_dir().unwrap_or_else(|| env::temp_dir()).join(".runst");
+    // TODO: use the app_dirs crate to get this in a more portable way
+
+    /// Directory where the Cargo workspace is located.
+    ///
+    /// Cargo.toml here will have the [workspace] section containing paths
+    /// to previously ran scripts.
+    static ref WORKSPACE_DIR: PathBuf = APP_DIR.join("workspace");
+}
 
 
 fn main() {
+    logging::init().unwrap();
+    trace!("runst {}", VERSION.map(|v| format!("v{}", v))
+        .unwrap_or_else(|| "(UNKNOWN VERSION)".to_owned()));
+
     let args: Vec<String> = env::args().skip(1).collect();
     if args.len() == 0 {
         panic!("No script name provided");
@@ -23,4 +57,21 @@ fn main() {
     // 5. cd && cargo run
     // The [workspace] thingie will allow for reusing compiled dependencies
     // via a single Cargo.lock
+}
+
+
+mod logging {
+    use log::SetLoggerError;
+    use slog::{self, DrainExt};
+    use slog_stdlog;
+    use slog_term;
+
+    use super::VERSION;
+
+    pub fn init () -> Result<(), SetLoggerError> {
+        let logger = slog::Logger::root(
+            slog_term::streamer().stderr().build().fuse(),
+            o!("version" => VERSION.unwrap_or("UNKNOWN")));
+        slog_stdlog::set_logger(logger)
+    }
 }
