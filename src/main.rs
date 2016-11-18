19672b2c81eb4360d1d88cab465a39f28d2998e9
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-11-17 22:34:37 -0800

    Mostly done with logging fiddling

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,13 @@
 
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate slog;
-             extern crate slog_stdlog;
+#[macro_use] extern crate slog_scope;
              extern crate slog_term;
-
-// `log` crate has to after `slog` as we want to use logging macros from `log`.
-#[macro_use] extern crate log;
 
 
 use std::env;
-use std::io::{self, Write};
 use std::path::PathBuf;
+use std::process::exit;
 
 
 lazy_static! {
@@ -36,17 +33,19 @@ lazy_static! {
 
 
 fn main() {
-    logging::init().unwrap();
-    trace!("runst {}", VERSION.map(|v| format!("v{}", v))
-        .unwrap_or_else(|| "(UNKNOWN VERSION)".to_owned()));
+    // TODO: parse command line flags, including logging verbosity
+    logging::init();
+    debug!("Initializing runst"; "version" => VERSION.unwrap_or("UNKNOWN"));
 
     let args: Vec<String> = env::args().skip(1).collect();
+    trace!("Parsing command line arguments"; "args" => format!("{:?}", args));
     if args.len() == 0 {
-        panic!("No script name provided");
+        error!("No script name provided");
+        exit(2);
     }
     let ref script = args[0];
 
-    writeln!(&mut io::stderr(), "Running {}...", script).unwrap();
+    info!("Running script"; "path" => *script);
 
     // TODO:
     // 1. create a directory for the cargo [workspace] if it doesn't exist
@@ -61,17 +60,22 @@ fn main() {
 
 
 mod logging {
-    use log::SetLoggerError;
-    use slog::{self, DrainExt};
-    use slog_stdlog;
+    use slog::{self, DrainExt, Level, Record};
+    use slog_scope;
     use slog_term;
 
-    use super::VERSION;
+    /// Initialize logging for the application.
+    pub fn init() {
+        // TODO: use slog_stream crate to better control log formatting;
+        // example: https://github.com/slog-rs/misc/blob/master/examples/global_file_logger.rs
+        let stderr = slog_term::streamer().sync().stderr()
+            .use_custom_timestamp(move |io| write!(io, ""));  // No log timestamps.
+        // TODO: accept a parameter to control logging verbosity
+        let drain = slog::filter(
+            |record: &Record| record.level().is_at_least(Level::Trace),
+            stderr.build());
 
-    pub fn init () -> Result<(), SetLoggerError> {
-        let logger = slog::Logger::root(
-            slog_term::streamer().stderr().build().fuse(),
-            o!("version" => VERSION.unwrap_or("UNKNOWN")));
-        slog_stdlog::set_logger(logger)
+        let logger = slog::Logger::root(drain.fuse(), o!());
+        slog_scope::set_global_logger(logger);
     }
 }
